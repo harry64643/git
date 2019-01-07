7f9aa6a9449a573aa29401853b5b705c286330c8
fowl <harry64643@gmail.com>
2019-01-07 15:28:41 +0800

    for++

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
-    int i ;
+    int i,j ;
+    int num,times ;
+
+    srand(time(NULL));
+
+    for(j=0;j<100;j++)
+    {
+        num=rand()%10+1;
+        if(num==1)
+        {
+            times++;
+        }
+    }
+
+    printf("%d\n",times);
 
     for(i=0;i<10;i++)
     {
